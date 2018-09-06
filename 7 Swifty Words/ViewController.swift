b1b96f546bfa4bb1b5328362e680673bf21ff18a
“Shimaa <a.moahy@wisyst.com>
2018-09-06 09:13:19 +0200

    Initial Commit

---
+++ 7 Swifty Words/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  7 Swifty Words
+//
+//  Created by admin on 9/6/18.
+//  Copyright © 2018 Creative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
